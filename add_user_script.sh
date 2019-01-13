b4ff24909e1f0eeeadfc1189191656e9509ee8db
yakir-jak <yakir6594@gmail.com>
2019-01-13 03:14:32 +0200

    added add_user_2

--- add_user_script.sh
+++ add_user_script.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 ###########################################################################################
 #
@@ -9,7 +9,16 @@
 #
 ###########################################################################################
 
+
+
 ###In the Script the user have 2 options to creat users. it can be a dingle user or a prepered list. (text file with names)
+
+### VARIABLES ###
+
+Choose_From_Links=cfl
+User_List=ul
+S_User=su
+
 echo "Select how you want to Add User : "
 ###Start in a simle Select menu 
 select OP in "Single_User" "Choose_From_List" ; 
@@ -18,41 +27,45 @@ do
 echo " you chose $OP " 
 sleep 1 
 ###Statement in case for list
-	if [ $OP == "$Choose_From_List" ];then
+	if [ $OP == "$cfl" ];then
 	echo "choose your List"
 ###Asking the user for a name of a file
-	read User_List
+	read -r ul
 ###Open a for loop for the chosen list
 	
-		for u in $( cat $User_List );
+		for u in $( cat $ul );
 		do
-		sudo useradd $u;
-			if [ $? == 9 ];then
+			sudo useradd $u;
+				if [ $? == 9 ];then
 ###Exit status 9 means the user existing
 ###Print if the NEW user already exist or successfully created
-			echo "###User $u Already Exist###"
-			continue
-			else 
-			echo "###The User $u  Added Successfully###"
-###Ending Statement
-			fi		
+				echo "###User $u Already Exist###"
+		#	continue
+				elif [ $? == 0 ];then
+				echo "###The User $u  Added Successfully###"
+#		else
+#		echo "thers a problem"
+###Ending Statement	
+				fi			
 		sleep 1
-
 		done
+				
+	fi
 ###Open A New Statemant fot "Single_User" option
 
-	if [ $OP == "Single_User" ];then
+	if [ $OP == "$Single_User" ];then
+	
 ###Asking from the user to type the name of the NEW user
 	echo "Type the name of the new user :"
-	read Singel_User
+	read -r SU
 
-	useradd $Single_User
+	sudo useradd $su
 	fi
 		if [ $? == 9 ];then
-		echo "###The User $Single_User Already Exist"
-		continue
+		echo "###The User $su Already Exist"
+	#	continue
 		else
-		echo "###The User $Singel_User Added Successfully"
+		echo "###The User $su Added Successfully"
 		fi
 echo "Done"
 done
